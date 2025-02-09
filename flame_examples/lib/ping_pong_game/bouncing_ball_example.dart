186bd22150143165f59615cbf3f4bc043ce63aab
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2025-02-09 19:10:35 +0530

    updating paddle position using DragCallbacks mixin

--- flame_examples/lib/ping_pong_game/bouncing_ball_example.dart
+++ flame_examples/lib/ping_pong_game/bouncing_ball_example.dart
@@ -4,25 +4,29 @@ import 'package:flame/game.dart';
 import 'package:flame_examples/ping_pong_game/components/ball_cmp.dart';
 import 'package:flame_examples/ping_pong_game/components/border_cmp.dart';
 import 'package:flame_examples/ping_pong_game/components/paddle_cmp.dart';
-import 'package:flutter/material.dart';
 
-class BouncingBallExample extends FlameGame with HasCollisionDetection, PanDetector {
-  // PanDetector
+class BouncingBallExample extends FlameGame with HasCollisionDetection { // PanDetector
   static const description = '''
     This example shows how you can use the Collisions detection api to know when a ball
     collides with the screen boundaries and then update it to bounce off these boundaries.
   ''';
-  late final Paddle paddle;
+  late final Paddle paddle, rightPaddle;
 
   @override
   void onLoad() {
     /// Initialize paddle at bottom center
     paddle = Paddle(
       position: Vector2(size.x / 2 - 50, size.y - 50),
-      size: Vector2(200, 20),
+      size: Vector2(100, 20),
+    );
+    rightPaddle = Paddle(
+      position: Vector2(size.x - 50, size.y / 2 - 50),
+      size: Vector2(20, 200),
+      isVertical: true,
     );
     addAll([
       paddle,
+     // rightPaddle,
       Ball(),
       ScreenHitbox(),
       BorderComponent(size),
@@ -34,10 +38,10 @@ class BouncingBallExample extends FlameGame with HasCollisionDetection, PanDetec
     ]);
   }
 
-  @override
-  void onPanUpdate(DragUpdateInfo info) {
-    super.onPanUpdate(info);
-    final touchX = info.eventPosition.global.x;
-    paddle.position.x = (touchX - paddle.width / 2).clamp(0, size.x - paddle.width);
-  }
+  // @override
+  // void onPanUpdate(DragUpdateInfo info) {
+  //   super.onPanUpdate(info);
+  //   final touchX = info.eventPosition.global.x;
+  //   paddle.position.x = (touchX - paddle.width / 2).clamp(0, size.x - paddle.width);
+  // }
 }
--- flame_examples/lib/ping_pong_game/components/paddle_cmp.dart
+++ flame_examples/lib/ping_pong_game/components/paddle_cmp.dart
@@ -3,9 +3,12 @@ import 'dart:ui';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/events.dart';
 
-class Paddle extends RectangleComponent with CollisionCallbacks { // DragCallbacks
-  Paddle({super.position, super.size, Color? paddleColor})
+class Paddle extends RectangleComponent with CollisionCallbacks, DragCallbacks, HasGameRef  {
+  final bool isVertical;
+
+  Paddle({super.position, super.size, Color? paddleColor, this.isVertical = false})
       : super(
           paint: Paint()..color = paddleColor ?? const Color(0xFFFFFFFF),
         );
@@ -16,20 +19,25 @@ class Paddle extends RectangleComponent with CollisionCallbacks { // DragCallbac
     add(RectangleHitbox());
   }
 
-  // @override
-  // void onDragStart(DragStartEvent event) {
-  //   super.onDragStart(event);
-  //   priority = 10;
-  // }
-  //
-  // @override
-  // void onDragEnd(DragEndEvent event) {
-  //   super.onDragEnd(event);
-  //   priority = 0;
-  // }
-  //
-  // @override
-  // void onDragUpdate(DragUpdateEvent event) {
-  //   position += event.localDelta;
-  // }
+  @override
+  void onDragStart(DragStartEvent event) {
+    super.onDragStart(event);
+    priority = 10;
+  }
+
+  @override
+  void onDragEnd(DragEndEvent event) {
+    super.onDragEnd(event);
+    priority = 0;
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+   // position += event.localDelta.;
+    if (isVertical) {
+      position.y = (position.y + event.localDelta.y).clamp(0, gameRef.size.y - height);
+    } else {
+      position.x = (position.x + event.localDelta.x).clamp(0, gameRef.size.x - width);
+    }
+  }
 }
